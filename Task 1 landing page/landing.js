28b03287b1b4566f80f46b6f25fdc7f913295940
mustafaaadil448 <156891490+mustafaaadil448@users.noreply.github.com>
2024-06-09 13:49:57 +0530

    Add files via upload

---
+++ Task 1 landing page/landing.js
@@ -0,0 +1,9 @@
+// Created by Md Mustufa
+
+/* song play  js part in html   */
+window.addEventListener("click",play);
+function play(){ document.getElementById("peace").play();
+}
+alert("Click on the screen to play song that i love 🤩");
+alert("Click on top stack photo to rotate the 4 photos together 🫶")
+alert(" I hope you would like it Thnaks for visit  ")
